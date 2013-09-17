fe1052f4b8f6488817c9b5a389f1d81f24ed133d
Jakob <jakob.boss@gmail.com>
2013-09-17 17:02:30 +0200

    Precompile css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ FikaPre::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.precompile += ['index.css']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
